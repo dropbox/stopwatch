1bdafb2e39ea078dd2e6e4d35094248336a14cfe
Nipunn Koorapati <nipunn@dropbox.com>
2016-01-26 22:50:25 -0800

    Add setup and tox

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "StopWatch",
+    version = "1.0",
+    description='Scoped, nested, aggregated python timing library',
+    keywords='stopwatch dropbox',
+    license='Apache License 2.0',
+    author='Nipunn Koorapati',
+    author_email='nipunn@dropbox.com',
+    py_modules=['stopwatch'],
+    url='https://github.com/dropbox/StopWatch',
+
+    install_requires = [],
+)
