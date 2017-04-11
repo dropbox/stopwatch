ffdf214f5ffbcfcda2e0c00e64c21607bcdba186
Nipunn Koorapati <nipunn@dropbox.com>
2017-04-11 15:44:56 -0700

    Update to version 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dbx-stopwatch",
-    version="1.1",
+    version="1.2",
     description='Scoped, nested, aggregated python timing library',
     keywords='stopwatch dropbox',
     license='Apache License 2.0',
@@ -10,7 +10,7 @@ setup(
     author_email='nipunn@dropbox.com',
     py_modules=['stopwatch', 'stopwatch_global'],
     url='https://github.com/dropbox/stopwatch',
-    download_url='https://github.com/dropbox/stopwatch/tarball/1.1',
+    download_url='https://github.com/dropbox/stopwatch/tarball/1.2',
 
     install_requires=[],
 )
