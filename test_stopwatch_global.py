a254e4b0b2bb137bc54bf415c82b13768ca3b973
rishabh <rishabh@dropbox.com>
2017-06-26 23:55:03 -0700

    Add test

--- test_stopwatch_global.py
+++ test_stopwatch_global.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 
 import pytest
 
+from mock import Mock
+
 from stopwatch_global import (
     global_sw,
     global_sw_del,
@@ -11,19 +13,29 @@ from stopwatch_global import (
 )
 
 
-def tracing_function(reported_traces):
-    pass
-
-
 @pytest.fixture
-def global_sw_fixture(request, tracing_func=tracing_function):
-    global_sw_init(export_tracing_func=tracing_func)
+def global_sw_fixture(request):
     request.addfinalizer(global_sw_del)
 
 
 @pytest.mark.usefixtures('global_sw_fixture')
 class TestStopWatchGlobal(object):
+    @staticmethod
+    def add_spans():
+        with global_sw().timer('parent', start_time=20, end_time=80):
+            global_sw().add_span_annotation('parent_annotation', 1)
+            with global_sw().timer('child', start_time=40, end_time=40):
+                global_sw().add_span_annotation('child_annotation', 1)
+
+    def test_reported_traces(self):
+        tracing_function = Mock()
+        global_sw_init(export_tracing_func=tracing_function)
+        self.add_spans()
+        reported_traces = global_sw().get_last_trace_report()
+        tracing_function.assert_called_once_with(reported_traces=reported_traces)
+
     def test_global_sw(self):
+        global_sw_init()
         with global_sw().timer('root'):
             pass
         last_report = global_sw().get_last_aggregated_report()
