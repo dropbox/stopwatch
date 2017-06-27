8b9a63d61fb48fbbdac38a97cb794a816d000db5
rishabh <rishabh@dropbox.com>
2017-06-27 15:38:26 -0700

    Use sorted and make function name consistent

--- test_stopwatch_global.py
+++ test_stopwatch_global.py
@@ -35,20 +35,20 @@ class TestStopWatchGlobal(object):
                 global_sw().add_span_annotation('child_annotation', 1)
 
     def test_callbacks(self):
-        tracing_function = Mock()
+        tracing_func = Mock()
         agg_timers_and_tracing_func = Mock()
-        global_sw_init(export_tracing_func=tracing_function,
+        global_sw_init(export_tracing_func=tracing_func,
                        export_aggregated_timers_and_tracing_func=agg_timers_and_tracing_func)
         self.add_spans()
         last_report = global_sw().get_last_aggregated_report()
         reported_traces = global_sw().get_last_trace_report()
 
-        assert last_report.aggregated_values.keys() == ['parent', 'parent#child']
+        assert sorted(last_report.aggregated_values.keys()) == ['parent', 'parent#child']
 
         assert len(reported_traces) == 2
         assert reported_traces[0].trace_annotations[0].key == 'child_annotation'
         assert reported_traces[1].trace_annotations[0].key == 'parent_annotation'
 
-        tracing_function.assert_called_once_with(reported_traces=reported_traces)
+        tracing_func.assert_called_once_with(reported_traces=reported_traces)
         agg_timers_and_tracing_func.assert_called_once_with(aggregated_report=last_report,
                                                             reported_traces=reported_traces)
